4c7d6ee8bcec34a9eaca4c3f5338d9d3dcee4636
Leonam Silva <leonam.murilo@gmail.com>
2022-07-28 10:53:33 -0300

    Os motores do React

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css';
 
+
+const posts = [{
+    id: 1,
+    author: {
+      avatar_url: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Sênior Web Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatar_url: "https://github.com/leonamjop.png",
+      name: "Leonam Silva",
+      role: "Front-End Developer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content:'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '👉 jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2022-05-10 14:30:00')
+  },
+]
+
 export function App() {
   return (
     <div>
@@ -13,10 +44,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Leonam Silva"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti in atque similique nihil reiciendis voluptates numquam, minima officia mollitia, molestias accusantium omnis possimus fugiat voluptas debitis velit perferendis sed quod."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,23 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0)
+
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/leonamjop.png"/>
@@ -15,17 +30,20 @@ export function Comment() {
                             <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:00'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button
+                            onClick={handleDeleteComment}
+                            title='Deletar comentário'
+                        >
                             <Trash size={24}/>
                         </button>
                     </header>
-                    <p>Muito bom Mayk, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp/>
-                        Aplaudir  <span>20</span>
+                        Aplaudir  <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
